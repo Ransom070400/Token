8dcce945f27a5c7b8f77559c9e3229713bde5ad6
Eze Ransom <148370144+Ransom070400@users.noreply.github.com>
2024-03-31 20:51:33 +0100

    Update deploy.js

--- script/deploy.js
+++ script/deploy.js
@@ -1,18 +1,18 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("CodeWithJoe", function () {
-  let CodeWithJoe;
-  let codeWithJoe;
+describe("RansomCoin", function () {
+  let RansomCoin;
+  let RansomCoin;
   let owner;
   let addr1;
   let addr2;
   let addrs;
 
   beforeEach(async function () {
-    CodeWithJoe = await ethers.getContractFactory("CodeWithJoe");
+    RansomCoin = await ethers.getContractFactory("RansomCoin");
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-    codeWithJoe = await CodeWithJoe.deploy();
+    RansomCoin = await CodeWithJoe.deploy();
     await codeWithJoe.deployed();
   });
 
@@ -30,23 +30,23 @@ describe("CodeWithJoe", function () {
 
   describe("Transactions", function () {
     it("Should transfer tokens between accounts", async function () {
-      await codeWithJoe.transfer(addr1.address, 50);
-      const addr1Balance = await codeWithJoe.balanceOf(addr1.address);
+      await RansomCoin.transfer(addr1.address, 50);
+      const addr1Balance = await RansomCoin.balanceOf(addr1.address);
       expect(addr1Balance).to.equal(50);
 
-      await codeWithJoe.connect(addr1).transfer(addr2.address, 50);
-      const addr2Balance = await codeWithJoe.balanceOf(addr2.address);
+      await RansomCoin.connect(addr1).transfer(addr2.address, 50);
+      const addr2Balance = await RansomCoin.balanceOf(addr2.address);
       expect(addr2Balance).to.equal(50);
     });
 
     it("Should fail if sender doesn’t have enough tokens", async function () {
-      const initialOwnerBalance = await codeWithJoe.balanceOf(owner.address);
+      const initialOwnerBalance = await RansomCoin.balanceOf(owner.address);
 
       await expect(
-        codeWithJoe.connect(addr1).transfer(owner.address, 1)
+        RansomCoin.connect(addr1).transfer(owner.address, 1)
       ).to.be.revertedWith("Not enough tokens");
 
-      expect(await codeWithJoe.balanceOf(owner.address)).to.equal(
+      expect(await RansomCoin.balanceOf(owner.address)).to.equal(
         initialOwnerBalance
       );
     });
